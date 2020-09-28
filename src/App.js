66c3be2a999d0f49054de0d280ac44cc435e55f8
Jesse Smart <jessemillersmart@gmail.com>
2020-09-28 16:48:30 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import List from "./components/List";
+
+class App extends Component {
+  render() {
+    const { store } = this.props;
+    return (
+      <main className="App">
+        <header className="App-header">
+          <h1>Trelloyes!</h1>
+        </header>
+        <div className="App-list">
+          {store.lists.map((list) => (
+            <List
+              key={list.id}
+              header={list.header}
+              cards={list.cardIds.map((id) => store.allCards[id])}
+            />
+          ))}
+        </div>
+      </main>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import store from './store.js'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App store={store} />
+
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
